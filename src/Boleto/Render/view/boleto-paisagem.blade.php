cb52f5633665ca306512f448384c9a6cdd4871dd
Fábio Souza <souzace@gmail.com>
2016-07-25 17:53:21 -0300

    formatação da tabela recibo do sacado

--- src/Boleto/Render/view/boleto-paisagem.blade.php
+++ src/Boleto/Render/view/boleto-paisagem.blade.php
@@ -4,89 +4,89 @@
     <meta charset="UTF-8">
     <title>{{ $beneficiario }}</title>
     <style>
-        .verticalTableHeader {
-            text-align:center;
-            white-space:nowrap;
+        .vertical-text {
+            transform-origin:50px 186px 0;
+            -webkit-transform: rotate(-90deg);
+            -moz-transform: rotate(-90deg);
+            -ms-transform: rotate(-90deg);
+            -o-transform: rotate(-90deg);
+            transform: rotate(-90deg);
+            filter: progid:DXImageTransform.Microsoft.BasicImage(rotation=3);
+            border:0;
+        }
+        
+        .linha-pontilhada{
+            color:#003;
+            font:9px Arial;
+            width:100%;
+            border-bottom:1px dashed #000;
+            text-align:right;
+            margin-bottom:10px;
             transform-origin:50% 50%;
             -webkit-transform: rotate(-90deg);
             -moz-transform: rotate(-90deg);
             -ms-transform: rotate(-90deg);
             -o-transform: rotate(-90deg);
             transform: rotate(-90deg);
-            
         }
-        .verticalTableHeader:before {
-            content:'';
-            padding-top:110%;/* takes width as reference, + 10% for faking some extra padding */
-            display:inline-block;
-            vertical-align:middle;
-        }
-
+        
     </style>
 </head>
 <body>
 <div style="float: left;">
     <table class="table-boleto" cellpadding="0" cellspacing="0" border="1">
-    <tr>
-        <td><img src="{{ $logo_banco_base64 }}" alt="logo do banco"></td>
-        <td>{{ $codigo_banco_com_dv }}</td>
-        <td>Recibo do Sacado</td>
-    </tr>
-    <tr>
-        <td>parcelar plano 1/2</td>
-        <td>vencimento {{ $data_vencimento }}</td>
-        <td rowspan="12" class="verticalTableHeader">
-            Autenticação Mecânica
-        </td>
-    </tr>
-    <tr>
-        <td colspan="2">agência / código cedente vencimento 01/08/10</td>
-        
-    </tr>
-    <tr>
-        <td colspan="2">Nosso Número 1000000000000</td>
-        
-    </tr>
-    <tr>
-        <td colspan="2">Número do documento 1000000000000</td>
-        
-    </tr>
-    <tr>
-        <td>especie moeda</td>
-        <td>quantidade</td>
-        
-    </tr>
-    <tr>
-        <td colspan="2">valor do documento</td>
-        
-    </tr>
-    <tr>
-        <td colspan="2">desconto abatimento</td>
-        
-    </tr>
-    <tr>
-        <td colspan="2">outras deduções</td>
-        
-    </tr>
-    <tr>
-        <td colspan="2">mora / multa</td>
-        
-    </tr>
-    <tr>
-        <td colspan="2">outros acrescimos</td>
-        
-    </tr>
-    <tr>
-        <td colspan="2">valor cobrado</td>
-        
-    </tr>
-    <tr>
-        <td colspan="2">sacado: <br /> cedente: <br /> CNPJ</td>
-        
-    </tr>
-    <tr>
-        <td colspan="3">mensagem</td>
-    </tr>
+        <tr>
+            <td><img src="{{ $logo_banco_base64 }}" alt="logo do banco"></td>
+            <td>{{ $codigo_banco_com_dv }}</td>
+            <td style="border-bottom: 1px solid black;">Recibo do Sacado</td>
+            <td rowspan="14">Corte na linha pontilhada</td>
+        </tr>
+        <tr>
+            <td>Parcelar Plano 1/2</td>
+            <td style="border-right: 1px solid black;">Vencimento {{ $data_vencimento }}</td>
+            <td rowspan="12" class="vertical-text">Autenticação Mecânica</td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border-right: 1px solid black;">Agência / Código Cedente {{ $agencia_codigo_beneficiario }}</td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border-right: 1px solid black;">Nosso Número {{ $nosso_numero_boleto }}</td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border-right: 1px solid black;">Número do documento {{ $numero_documento }}</td>
+        </tr>
+        <tr>
+            <td>Espécie Moeda {{ $especie }}</td>
+            <td style="border-right: 1px solid black;">Quantidade {{ $quantidade }}</td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border-right: 1px solid black;">1 (=) Valor do Documento {{ $valor_documento }}</td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border-right: 1px solid black;">2 (-) Desconto / Abatimento</td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border-right: 1px solid black;">3 (-) Outras Deduções</td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border-right: 1px solid black;">4 (+) Mora / Multa</td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border-right: 1px solid black;">5 (+) Outros Acréscimos</td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border-right: 1px solid black;">6 (=) Valor Cobrado</td>
+        </tr>
+        <tr>
+            <td colspan="2" style="border-right: 1px solid black;">Sacado: {{ $pagador }}<br /> cedente: {{ $beneficiario }}<br /> CNPJ: {{ $beneficiario_cpf_cnpj }}</td>
+        </tr>
+        <tr>
+            <td colspan="3" style="border-right: 1px solid black;">
+            {{ $demonstrativo[0] }} <br />
+            {{ $demonstrativo[1] }} <br />
+            {{ $demonstrativo[2] }} <br />
+            </td>
+        </tr>
     </table>
 </div>
 <div style="width: 666px: float: left;">
